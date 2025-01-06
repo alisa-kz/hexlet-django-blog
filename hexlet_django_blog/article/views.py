f89c55389218bdc622b36afb579eac94d58e690c
Alisa Kuzyagina <khairutdinovaaalisa@gmail.com>
2025-01-07 00:08:51 +0300

    add forms

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,15 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 # from django.http import HttpResponse
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
+from django.contrib import messages
 
-# def index(request):
-#     name = 'article'
-#     return render(
-#         request,
-#         'articles_index.html',
-#         context={'name': name}
-#     )
 
 class IndexView(View):
 
@@ -26,4 +21,59 @@ class ArticleView(View):
 
     def get(self, request, *args, **kwargs):
         article = get_object_or_404(Article, id=kwargs['id'])
-        return render(request, 'articles/show.html', context={'article': article})
+        return render(
+            request,
+            'articles/show.html',
+            context={'article': article}
+            )
+
+
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, "articles/create.html", {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, "Article added")
+            return redirect("article_index")
+        messages.add_message(request, messages.ERROR, "Wrong data")
+        return render(request, "articles/create.html", {"form": form})
+
+
+class ArticleFormEditView(View):
+
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(
+            request, "articles/update.html", {"form": form, "article_id": article_id}
+        )
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, "Article updated")
+            return redirect("article_index")
+        messages.add_message(request, messages.ERROR, "Wrong data")
+        return render(
+            request, "articles/update.html", {"form": form, "article_id": article_id}
+        )
+
+
+class ArticleFormDeleteView(View):
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        if article:
+            article.delete()
+            messages.add_message(request, messages.SUCCESS, "Article delited")
+        return redirect("article_index")
